d29070cb1fdc34c29c4233f52fe451fd271bf9bb
Ricky <72452852+rnunez22@users.noreply.github.com>
2020-10-08 20:01:24 -0600

    Initial commit

---
+++ src/com/ricardo/Main.java
@@ -0,0 +1,9 @@
+package com.ricardo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //The program will display my name
+        System.out.println("Hello, my name is Ricardo.");
+    }
+}
