3a7a1d599689289c6bbeddd3adfd9af69c7c5980
Chad L <72481913+clookcode@users.noreply.github.com>
2020-10-10 11:19:20 -0400

    adding my hello line

--- src/com/ricardo/Main.java
+++ src/com/ricardo/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //The program will display my name
         System.out.println("Hello, my name is Ricardo.");
+        //hello Ricardo
+        System.out.println("Hello Ricardo, my name is Chad.");
     }
 }
